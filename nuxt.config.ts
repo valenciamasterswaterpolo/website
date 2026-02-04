03d18ab4dc7cc890eeb41eb78237fe57a81dac94
Marcos Sanz Latorre <marcossanzlatorre@gmail.com>
2026-02-04 22:39:13 +0100

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,11 +67,27 @@ export default defineNuxtConfig({
         { rel: 'manifest', href: '/site.webmanifest' },
         // SEO & Fonts
         { rel: 'canonical', href: 'https://valenciamasterswaterpolo.com' },
+        // Hreflang tags for multilingual support
+        { rel: 'alternate', hreflang: 'en', href: 'https://valenciamasterswaterpolo.com' },
+        { rel: 'alternate', hreflang: 'es', href: 'https://valenciamasterswaterpolo.com' },
+        { rel: 'alternate', hreflang: 'x-default', href: 'https://valenciamasterswaterpolo.com' },
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Poppins:wght@300;400;500;600;700&display=swap' }
       ],
+      noscript: [
+        // Google Tag Manager (noscript)
+        { innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MW9XM7H2" height="0" width="0" style="display:none;visibility:hidden"></iframe>' }
+      ],
       script: [
+        // Google Tag Manager
+        {
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MW9XM7H2');`
+        },
         // WebSite schema for sitelinks search box
         {
           type: 'application/ld+json',
@@ -82,6 +98,64 @@ export default defineNuxtConfig({
             'url': 'https://valenciamasterswaterpolo.com',
             'description': 'III Masters Water Polo Tournament in Valencia, Spain',
             'inLanguage': ['en', 'es']
+          })
+        },
+        // SportsEvent schema for the tournament
+        {
+          type: 'application/ld+json',
+          innerHTML: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'SportsEvent',
+            'name': 'Valencia Masters Water Polo Cup 2026',
+            'description': 'III Masters Water Polo Tournament in Valencia, Spain - Join teams from across Europe for competitive water polo and Mediterranean vibes.',
+            'startDate': '2026-05-16',
+            'endDate': '2026-05-17',
+            'eventStatus': 'https://schema.org/EventScheduled',
+            'eventAttendanceMode': 'https://schema.org/OfflineEventAttendanceMode',
+            'location': {
+              '@type': 'Place',
+              'name': 'UPV Campus - Universitat Politècnica de València',
+              'address': {
+                '@type': 'PostalAddress',
+                'streetAddress': 'Camí de Vera, s/n',
+                'addressLocality': 'Valencia',
+                'addressRegion': 'Comunidad Valenciana',
+                'postalCode': '46022',
+                'addressCountry': 'ES'
+              },
+              'geo': {
+                '@type': 'GeoCoordinates',
+                'latitude': 39.4817,
+                'longitude': -0.3419
+              }
+            },
+            'image': 'https://valenciamasterswaterpolo.com/images/og_image.jpg',
+            'organizer': {
+              '@type': 'Organization',
+              'name': 'Valencia Masters Water Polo Cup',
+              'url': 'https://valenciamasterswaterpolo.com'
+            },
+            'sport': 'Water Polo',
+            'url': 'https://valenciamasterswaterpolo.com',
+            'inLanguage': ['en', 'es']
+          })
+        },
+        // Organization schema
+        {
+          type: 'application/ld+json',
+          innerHTML: JSON.stringify({
+            '@context': 'https://schema.org',
+            '@type': 'Organization',
+            'name': 'Valencia Masters Water Polo Cup',
+            'url': 'https://valenciamasterswaterpolo.com',
+            'logo': 'https://valenciamasterswaterpolo.com/images/logo.png',
+            'description': 'Organizers of the annual Masters Water Polo Tournament in Valencia, Spain',
+            'address': {
+              '@type': 'PostalAddress',
+              'addressLocality': 'Valencia',
+              'addressCountry': 'ES'
+            },
+            'sameAs': []
           })
         }
       ]
